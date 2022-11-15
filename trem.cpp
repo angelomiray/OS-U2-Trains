c6f7902ce2de5d3a84ec700f87965198cf9344cf
angelomiray <angelotiufrn@gmail.com>
2022-11-15 17:34:33 -0300

    Trilhos e 5 trens implementados, mas sem mutexes, semaforos e configurações de manipulação de velocidades

--- trem.cpp
+++ trem.cpp
@@ -6,37 +6,84 @@ Trem::Trem(int ID, int x, int y){
     this->ID = ID;
     this->x = x;
     this->y = y;
-    velocidade = 100;
+    velocidade = 50;
 }
 
 //Função a ser executada após executar trem->START
 void Trem::run(){
     while(true){
         switch(ID){
-        case 1:     //Trem 1
-            if (y == 30 && x <330)
-                x+=10;
-            else if (x == 330 && y < 150)
-                y+=10;
-            else if (x > 60 && y == 150)
-                x-=10;
-            else
-                y-=10;
-            emit updateGUI(ID, x,y);    //Emite um sinal
-            break;
-        case 2: //Trem 2
-            if (y == 30 && x <600)
-                x+=10;
-            else if (x == 600 && y < 150)
-                y+=10;
-            else if (x > 330 && y == 150)
-                x-=10;
-            else
-                y-=10;
-            emit updateGUI(ID, x,y);    //Emite um sinal
-            break;
-        default:
-            break;
+            case 1:     //Trem 1
+                if (x == 60 && y > 30) {
+                    y == 34 ? y -=4 : y -= 10; //limite com base no tamanho do trem (quadrado) e o posicionamento inicial nos trilhos
+                }
+                else if (y == 30 && x < 330) {
+                    x += 10;
+                }
+                else if (x == 330 && y < 150)
+                    y += 10;
+                else
+                    x -= 10;
+
+                emit updateGUI(ID, x, y);    //Emite um sinal
+                break;
+            case 2: //Trem 2
+                if (y == 30 && x < 600)
+                    x == 595 ? x += 5 : x += 10; //limite com base no tamanho do trem (quadrado) e o posicionamento inicial nos trilhos
+                else if (x == 600 && y < 150)
+                    y += 10;
+                else if (y == 150 && x > 330)
+                    x -= 10;
+                else
+                    y -= 10;
+                emit updateGUI(ID, x, y);    //Emite um sinal
+                break;
+            case 3: //Trem 3
+                if (x == 870 && y < 150)
+                    y == 144 ? y += 6 : y += 10;
+                else if (y == 150 && x > 600)
+                    x -= 10;
+                else if (x == 600 && y > 30)
+                    y -= 10;
+                else
+                    x += 10;
+                emit updateGUI(ID, x, y);    //Emite um sinal
+                break;
+            case 4: //Trem 4
+                if (x == 195 && y > 150) {
+                    if (y == 156) {
+                        y -= 6;
+                    } else if (y == 157) {
+                        y -= 7;
+                    } else {
+                        y -= 10;
+                    }
+                }
+                else if (y == 150 && x < 465)
+                    x += 10;
+                else if (x == 465 && y < 287)
+                    y == 280 ? y += 7 : y += 10;
+                else
+                    x -= 10;
+                emit updateGUI(ID, x, y);    //Emite um sinal
+                break;
+            case 5: //Trem 5
+                if (y == 287 && x > 465) {
+                    if (x == 470) x -= 5;
+                    else x -= 10;
+                }
+                else if (x == 465 && y > 150)
+                    if (y == 157) y -= 7;
+                    else y -= 10;
+                else if (y == 150 && x < 735)
+                    x += 10;
+                else
+                    if (y == 280) y += 7;
+                    else y+= 10;
+                emit updateGUI(ID, x, y);    //Emite um sinal
+                break;
+            default:
+                break;
         }
         msleep(velocidade);
     }
--- trem.h
+++ trem.h
@@ -11,21 +11,20 @@
  *
 */
 class Trem: public QThread {
- Q_OBJECT
-public:
-    Trem(int,int,int);  //construtor
-    void run();         //função a ser executada pela thread
+     Q_OBJECT
+    public:
+        Trem(int,int,int);  //construtor
+        void run();         //função a ser executada pela thread
 
+    //Cria um sinal
+    signals:
+        void updateGUI(int,int,int);
 
-//Cria um sinal
-signals:
-    void updateGUI(int,int,int);
-
-private:
-   int x;           //posição X do trem na tela
-   int y;           //posição Y do trem na tela
-   int ID;          //ID do trem
-   int velocidade;  //Velocidade. É o tempo de dormir em milisegundos entre a mudança de posição do trem
+    private:
+       int x;           //posição X do trem na tela
+       int y;           //posição Y do trem na tela
+       int ID;          //ID do trem
+       int velocidade;  //Velocidade. É o tempo de dormir em milisegundos entre a mudança de posição do trem
 };
 
 #endif // TREM_H
